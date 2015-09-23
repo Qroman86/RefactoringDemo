8fb88f073c8c5862d953165171edeb80834b4b0d
igolovaschenko <igolovaschenko@ATC-10824.at-consulting.ru>
2015-09-23 12:37:36 +0300

    new edition

--- src/test/java/atc/CustomerTest.java
+++ src/test/java/atc/CustomerTest.java
@@ -18,6 +18,32 @@ public class CustomerTest {
 	Movie stubMovie;
 	Rental stubRental;
 
+	private Movie generateStubMovie(String title, PriceCodes priceCode) {
+		Movie stubMovieLocal = mock(Movie.class);
+		when(stubMovieLocal.getTitle()).thenReturn(title);
+		when(stubMovieLocal.getPriceCode()).thenReturn(priceCode);
+		return stubMovieLocal;
+	}
+
+	private Rental generateStubRental(Movie stubMovieLocal, int numOfDaysRented) {
+		Rental stubRentalLocal = mock(Rental.class);
+		when(stubRentalLocal.getMovie()).thenReturn(stubMovieLocal);
+		when(stubRentalLocal.getDaysRented()).thenReturn(numOfDaysRented);
+		return stubRentalLocal;
+	}
+
+	private void universalShouldTotalAmountEqualToEtalonWhenStatementPrintedForSomePriceCode(String title,
+			String customerName, PriceCodes priceCode, int numOfDaysRented, String etalonAmount) {
+
+		Movie stubMovieLocal = generateStubMovie(title, priceCode);
+		Rental stubRentalLocal = generateStubRental(stubMovieLocal, numOfDaysRented);
+		Customer sosut = new Customer(customerName);
+		// system object stays under test (sosut)
+		sosut.addRental(stubRentalLocal);
+		System.out.println(sosut.Statement());
+		org.fest.assertions.Assertions.assertThat(sosut.Statement()).contains("Amount owed is " + etalonAmount);
+	}
+
 	@Before
 	public void setUp() {
 
@@ -31,13 +57,35 @@ public class CustomerTest {
 
 	@Test
 	public void shouldGetDaysRentedCalledTwiceWhenStatementMethodCalled() {
+		// system object stays under test (sosut)
+		Customer sosut = new Customer("Mickey Mouse");
 
-		Customer sut = new Customer("Mickey Mouse");
+		sosut.addRental(stubRental);
 
-		sut.addRental(stubRental);
-
-		sut.Statement();
+		sosut.Statement();
 
 		verify(stubRental, times(2)).getDaysRented();
+	}
+
+	@Test
+	public void shouldTotalAmountEqualToEtalonWhenStatementPrintedForRegularPriceCode() {
+
+		universalShouldTotalAmountEqualToEtalonWhenStatementPrintedForSomePriceCode("Cinderella", "Mickey Mouse",
+				PriceCodes.Childrens, 5, "3.0");
+	}
+
+	@Test
+	public void shouldTotalAmountEqualToEtalonWhenStatementPrintedForNewReleasePriceCode() {
+
+		universalShouldTotalAmountEqualToEtalonWhenStatementPrintedForSomePriceCode("Cinderella", "Mickey Mouse",
+				PriceCodes.NewRelease, 5, "15.0");
+	}
+
+	@Test
+	public void shouldTotalAmountEqualToEtalonWhenStatementPrintedForChildrensPriceCode() {
+
+		universalShouldTotalAmountEqualToEtalonWhenStatementPrintedForSomePriceCode("Cinderella", "Mickey Mouse",
+				PriceCodes.Childrens, 5, "3.0");
+
 	}
 }
